bad24244d45d314f5c8e53f3e532525372bd1178
autologrossa <autologros.sa@gmail.com>
2026-06-15 10:25:40 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
-import React from 'react';
+jsimport React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<React.StrictMode><App /></React.StrictMode>);
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
