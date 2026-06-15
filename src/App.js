6ebcd2561b241e3a22dac5e91c7cf145b97c66c1
autologrossa <autologros.sa@gmail.com>
2026-06-15 14:21:59 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import { Routes, Route } from 'react-router-dom';
 import FirmaCliente from './FirmaCliente';
 import LegajoDigital, { PanelLegajos } from './LegajoDigital';
 import ModuloE from './ModuloE_Desembolso';
-import ModuloF, { CuentaCorriente } from './ModuloF_Cartera';
+import ModuloF from './ModuloF_Cartera';
+import { CuentaCorriente } from './ModuloF_Cartera';
+
 import ModuloH from './ModuloH_Reportes';
 import { db } from './supabase';
  
